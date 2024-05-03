8b3dcf24e4176424be8b372eca433f2bea9d8e5f
Nicole Brubaker <nschumacher196@gmail.com>
2024-05-02 21:22:28 -0700

    fixing mistake in product model, putting PUT back

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,6 +34,44 @@ router.get("/:id", async (req, res) => {
 //       stock: 3,
 //       tagIds: [1, 2, 3, 4]
 //     }
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedProduct = await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (req.body.tagIds && req.body.tagIds.length) {
+      const productTags = await ProductTag.findAll({
+        where: { product_id: req.params.id },
+      });
+      const productTagIds = productTags.map(({ tag_id }) => tag_id);
+
+      const newProductTags = req.body.tagIds
+        .filter((tag_id) => !productTagIds.includes(tag_id))
+        .map((tag_id) => {
+          return {
+            product_id: req.params.id,
+            tag_id,
+          };
+        });
+
+      const productTagsToRemove = productTags
+        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .map(({ id }) => id);
+
+      await Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+      ]);
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 router.post("/", async (req, res) => {
   try {
